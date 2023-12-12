2800b0959de6ce04530da1d62774966b02626fd1
Асылхан Туран <aturan@alfabank.ru>
2023-12-12 15:47:35 +0600

    Initial Commit

---
+++ eBaton-timer/eBaton_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  eBaton_timerApp.swift
+//  eBaton-timer
+//
+//  Created by Assylkhan Turan on 12.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct eBaton_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
